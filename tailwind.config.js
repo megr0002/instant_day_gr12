445c79c95bb51b4c22cede9af20bdd524787cd0b
Lea Nørager <leno0001@stud.kea.dk>
2025-09-19 12:07:53 +0200

    add below link

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        tertiary: {
+          400: '#ba0606',
+        },
+        // ...other colors
+      },
+    },
+  },
+}
